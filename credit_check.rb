7550175f2432c889db129631e17e02d741e5e58a
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-11 20:07:06 -0600

    wraps into methods

--- credit_check.rb
+++ credit_check.rb
@@ -3,41 +3,54 @@ card_number = "5541801923795240"  #invalid
 # card_number = gets.chomp                      #to check inputed card numbers for validity
 
 class CreditCheck
+  attr_reader :digits
 
-card_array = card_number.split(//)
-card_array.map! do |x|
-  x.to_i
-end
-
-card_array = card_array.reverse
-
-card_array.map!.with_index do |x, i|
-  if i.odd? == true
-    x * 2
-  elsif i.even? == true
-    x
-  else
+  def initialize(digits)
+    @digits = digits
   end
-end
 
-card_array.map!.with_index do |x, i|
-  if x > 9
-    digit_sum = []
-    digit_sum << card_array.slice(i).to_s.split(//)
-  else
-    x
+
+  def convert_to_integer
+    card_array = card_number.split(//)
+    card_array.map! do |x|
+      x.to_i
+    end
   end
-end
 
-card_array = card_array.flatten!
-card_array.map! do |x|
-  x.to_i
-end
+  def multiply_ev_other_by_2
+    card_array = card_array.reverse
+    card_array.map!.with_index do |x, i|
+      if i.odd? == true
+        x * 2
+      elsif i.even? == true
+        x
+      else
+      end
+    end
+  end
 
-if card_array.inject(:+) % 10 == 0
-  puts "The card is valid!"
-else
-  puts "The card is not valid!"
-end
+  def checks_for_double_digits
+    card_array.map!.with_index do |x, i|
+      if x > 9
+        digit_sum = []
+        digit_sum << card_array.slice(i).to_s.split(//)
+      else
+        x
+      end
+    end
+  end
+
+
+  def checks_sum_of_digits
+    card_array = card_array.flatten!
+    card_array.map! do |x|
+      x.to_i
+    end
+    if card_array.inject(:+) % 10 == 0
+      puts "The card is valid!"
+    else
+      puts "The card is not valid!"
+    end
+  end
 
 end
